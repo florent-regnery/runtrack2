67c56d6d47664b96e47b6079808bbce54169983c
flow <91847558+florent-regnery@users.noreply.github.com>
2021-11-02 15:19:42 +0100

    job02

---
+++ job02/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>job02</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+
+<?php     
+
+                                //    string1
+$nom = " Hello LaPlateforme!";
+
+echo "<br>";
+
+                                // string2
+
+
+$nom1 = "Hello LaPlateforme!";
+
+echo $nom, "<br>", $nom1;
+
+
+?>
