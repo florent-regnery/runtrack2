c4483bf4502cdc6a737494c9dbe5ed82b9c12d92
flow <91847558+florent-regnery@users.noreply.github.com>
2021-11-03 00:42:46 +0100

    Add files via upload

---
+++ jour01/job01/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>job01</title>
+</head>
+<body>
+
+<?php
+                            // strings
+                            
+$str= "LaPlateforme";
+        
+
+$str2= "Vive";
+
+
+$str3= "!";
+                    //    ordre de lecture
+
+
+echo $str2." ". $str." ". $str3;
+
+echo "<br>";
+
+
+                    // Valeur
+
+$val= 6 ;
+
+echo $val;
+
+echo "<br>";
+
+                    // addition de valeur
+$val2= 4 ;
+
+$val3 = ($val ) + 4 ;
+
+echo $val3 ;
+
+echo "<br>";
+
+                    // Bool?
+
+$myBool = true ;
+
+echo $myBool;
+
+echo "<br>";
+                    // n'apparait pas
+
+$myBool = false ;
+
+echo $myBool;
+
+
+
+?>
+    
+</body>
+</html>
+
+
